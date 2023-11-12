d16049ca671a861ae9cde39d22e50c2598f3e67a
nguyentranluanit@gmail.com <nguyentranluanit@gmail.com>
2023-11-12 09:42:10 +0700

    mvc

---
+++ model/DBConfig.php
@@ -0,0 +1,40 @@
+<?php
+    class Database {
+        private $conn = NUll;
+        private $result = NULL;
+        public function connect() {
+            $this->conn = new mysqli("localhost", "root", "", "onlinesellingdb");
+            if (!$this->conn) {
+                echo "Kết nối thất bại";
+                exit();
+            } else {
+                mysqli_set_charset($this->conn,"utf-8");
+            }
+        }
+
+        // thuc thi cau truy van
+        public function execute($sql) {
+            $this->result = $this->conn->query($sql);
+            return $this->result;
+        }
+
+        // phuong thuc lay du lieu
+        public function getData() {
+            if ($this->result) {
+                $data = $this->result->fetch_assoc();
+            }
+        }
+
+        // phuong thuc lay toan bo du lieu
+        public function getAll() {
+            
+        }
+
+        // phuong thuc dem so ban ghi
+
+        // phuong thuc them du lieu
+        public function insertData() {
+            
+        }
+    }
+?>
