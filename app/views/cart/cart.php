7172a8ef3b00a1717db4867c2d3c081ecd85720e
Dinh Van <trankyvan08@gmail.com>
2023-11-14 00:36:21 +0700

    Thiet ke trang cart

---
+++ app/views/cart/cart.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Giỏ Hàng</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+        th, td {
+            text-align: center;
+            padding: 12px;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+        input[type="number"] {
+            width: 50px;
+        }
+        input[type="submit"] {
+            padding: 8px 16px;
+            background-color: #4CAF50;
+            border: none;
+            color: white;
+            cursor: pointer;
+        }
+    </style>
+</head>
+<body>
+    <h2>Giỏ Hàng</h2>
+    
+    <table>
+        <tr>
+            <th>Sản phẩm</th>
+            <th>Đơn giá</th>
+            <th>Số lượng</th>
+            <th>Số tiền</th>
+            <th>Thao tác</th>
+        </tr>
+        
+    </table>
+
+    <p>Giỏ hàng của bạn đang trống.</p>
+</body>
+</html>
