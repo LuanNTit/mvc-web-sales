c006a306b4d34139ead5dcf506e7f4010d5d4c90
Nguyen Tran Luan <nguyentranluanit@gmail.com>
2023-11-22 02:06:40 +0700

    routes cho web va them file Dashboard.php Controller Dashboard cua admin

---
+++ app/controller/admin/partner/Dashboard.php
@@ -0,0 +1,10 @@
+<?php
+class Dashboard {
+    public function index() {
+        echo 'Trang Dashboard';
+    }
+    public function detail() {
+        echo 'Trang chi tiet Dashboard';
+    }
+}
+?>
--- configs/routes.php
+++ configs/routes.php
@@ -1,3 +1,16 @@
 <?php
     $routes['default_controller'] = 'home';
+    /**
+     * Đường dẫn ảo => Đường dẫn thật
+     * 
+    */
+    
+    $routes['san-pham'] = 'product/index';
+    $routes['trang-chu'] = 'home';
+    $routes['dang-nhap'] = 'account/login';
+    $routes['dang-ky'] = 'account/signup';
+    $routes['gio-hang'] = 'cart';
+    $routes['thanh-toan/.+-(\d+)'] = 'checkout/index/$1';
+    $routes['san-pham/.+-(\d+)'] = 'product/detail/$1';
+    // $routes['don-hang'] = 'order';
 ?>
