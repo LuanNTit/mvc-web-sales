933a5bb8161a07241ee1196859c2b96095aeed81
Nguyen Tran Luan <nguyentranluanit@gmail.com>
2023-11-25 12:43:52 +0700

    them model cho category, product lan luot trang home va product

--- app/models/ProductModel.php
+++ app/models/ProductModel.php
@@ -2,9 +2,16 @@
 class ProductModel {
     public function getProductLists() {
         return [
-            'Sản phẩm 1',
-            'Sản phẩm 2',
-            'Sản phẩm 3'
+            [
+                "url_img" => _WEB_ROOT."/public/assets/clients/images/sanpham1.png",
+                "name" => "Women's Blouse Top",
+                "price" => 53
+            ],
+            [
+                "url_img" => _WEB_ROOT."/public/assets/clients/images/sanpham2.png",
+                "name" => "Men's Jacket",
+                "price" => 75.55
+            ]
         ];
     }
 
